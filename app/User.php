bf70ff6b6c97252b372413ee5d1bfe48e385b25b
minami.mori <minamimori0604@gmail.com>
2018-06-21 05:18:20 +0000

    Procfile

--- app/User.php
+++ app/User.php
@@ -9,20 +9,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -73,6 +63,51 @@ class User extends Authenticatable
             return $item_id_exists;
         } else {
             $item_code_exists = $this->want_items()->where('code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
+    
+// kokokara have
+     public function have_items()
+    {
+        return $this->items()->where('type', 'have');
+    }
+
+    public function have($itemId)
+    {
+        $exist = $this->is_having($itemId);
+
+        if ($exist) {
+            // do nothing
+            return false;
+        } else {
+            // do "want"
+            $this->items()->attach($itemId, ['type' => 'have']);
+            return true;
+        }
+    }
+
+    public function dont_have($itemId)
+    {
+        // Is the user already "want"?
+        $exist = $this->is_having($itemId);
+
+        if ($exist) {
+            // remove "want"
+            \DB::delete("DELETE FROM item_user WHERE user_id = ? AND item_id = ? AND type = 'want'", [\Auth::id(), $itemId]);
+        } else {
+            // do nothing
+            return false;
+        }
+    }
+
+    public function is_having($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)) {
+            $item_id_exists = $this->have_items()->where('item_id', $itemIdOrCode)->exists();
+            return $item_id_exists;
+        } else {
+            $item_code_exists = $this->have_items()->where('code', $itemIdOrCode)->exists();
             return $item_code_exists;
         }
     }
